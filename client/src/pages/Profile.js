53586f2151d36e0c120984fe043705ee277c362d
jester <jaimebuddy98@gmail.com>
2022-04-13 15:04:30 -0500

    user authintication and token/local storage  functionaluty

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
+
 import ThoughtList from '../components/ThoughtList';
 import FriendList from '../components/FriendList';
 
 import { useQuery } from '@apollo/client';
-import { QUERY_USER } from '../utils/queries';
+import { QUERY_USER, QUERY_ME } from '../utils/queries';
 
 const Profile = () => {
   const { username: userParam } = useParams();
